0c2e50433f8fe355028d09ec76bb8b9905bc2762
gabrieldernbach <gabriel.dernbach@gmail.com>
2024-09-11 14:19:36 +0200

    Update flow_matching.py to all in one class

--- flow_matching.py
+++ flow_matching.py
@@ -2,17 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-from einops.layers.torch import Rearrange
 from tqdm import tqdm
+from einops.layers.torch import Rearrange
 import matplotlib.pyplot as plt
 
-
-def time_embedding(time: torch.tensor, embedding_dim: int):
-    half_dim = embedding_dim // 2
-    emb = torch.log(torch.tensor(10000.0, device=time.device)) / (half_dim - 1)
-    emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=time.device) * -emb)
-    emb = emb * time.float()
-    return torch.cat([torch.sin(emb), torch.cos(emb)], dim=-1)
+def sinusoidal_embedding(x, dim):
+    # x should be vector of [num_samples] in [0, 1]
+    frequencies = torch.linspace(
+        torch.log(torch.tensor(1.0)),
+        torch.log(torch.tensor(1000.0)),
+        dim // 2, device=device).exp()
+    angular_speeds = frequencies.reshape(-1, 1).mul(torch.pi*2.)
+    embeddings = torch.cat([
+        angular_speeds.mul(x).sin(),
+        angular_speeds.mul(x).cos(),
+    ], axis=0)
+    return embeddings.T
 
 def mlp(ins, hidden, outs):
     return nn.Sequential(
@@ -21,8 +26,8 @@ def mlp(ins, hidden, outs):
         nn.Linear(hidden, outs),
     )
 
-class ResidualBlock(nn.Module):
-    '''see https://arxiv.org/pdf/2212.09748'''
+class ConditionalResidualBlock(nn.Module):
+    '''see https://arxiv.org/pdf/2212.09748 for adaptive layer norm feature-wise modulation'''
     def __init__(self, noise_dimension, condition_dimension, latent_dimension, num_blocks):
         super().__init__()
         self.conditioning_layer = mlp(
@@ -42,14 +47,22 @@ class ResidualBlock(nn.Module):
         x = F.layer_norm(x, [x.shape[-1]])
         sss = self.conditioning_layer(condition)
         scale1, shift, scale2 = sss.chunk(3, dim=-1)
-        x = self.mlp(scale1 * x + shift) * scale2
+        x = self.mlp((1+scale1) * x + shift) * (1+scale2)
         return x/self.num_blocks + residual
 
-class DenoisingMLP(nn.Module):
-    def __init__(self, noise_dimension, condition_dimension, num_blocks, latent_dimension):
+
+class ConditionalFlow(nn.Module):
+    def __init__(self, 
+                 noise_dimension,
+                 condition_dimension, 
+                 num_blocks, 
+                 latent_dimension,
+                 num_classes,
+                ):
         super().__init__()
+        self.noise_dimension = noise_dimension
         self.blocks = nn.ModuleList([
-            ResidualBlock(
+            ConditionalResidualBlock(
                 noise_dimension=noise_dimension, 
                 condition_dimension=condition_dimension,
                 latent_dimension=latent_dimension,
@@ -57,22 +70,49 @@ class DenoisingMLP(nn.Module):
             ) 
             for _ in range(num_blocks)
         ])
+        self.cls_emb = nn.Sequential(
+            nn.Embedding(num_classes, latent_dimension),
+            nn.SiLU(),
+            nn.Linear(latent_dimension, condition_dimension),
+        )
         
-    def forward(self, x, timestep, class_token):
-        cond = class_token + time_embedding(timestep, z.size(1))
+    def forward(self, x, time, cls_idx):
+        cls_emb = self.cls_emb(cls_idx)
+        t_embedding = sinusoidal_embedding(time.squeeze(1), cls_emb.size(-1))
         for block in self.blocks:
-            x = block(x, cond)
+            x = block(x, cls_emb + t_embedding)
         return x
+
+    def loss(self, x, cls_idx):
+        noise = torch.randn(x.shape, device=device)    
+        time = torch.randn(size=(len(x),1), device=device).sigmoid()
+        noised = (1-time) * x + (0.001 + 0.999*time) * noise
+        pred = self.forward(noised, time, cls_idx)
+        return F.mse_loss(pred, noise.mul(0.999).sub(x))
+
+    def sample(self, cls_idx, n_steps=100):
+        # cls_idx should be a vector of shape [num_samples]
+        x = torch.randn(len(cls_idx), self.noise_dimension, device=cls_idx.device)
+        dt = 1.0 / n_steps
+        with torch.no_grad(): # runge-kutta-4 solve diffeq
+            for t in tqdm(torch.linspace(1, 0, n_steps, device=x.device)):
+                t = t.expand(len(x), 1)
+                k1 = self.forward(x, t, cls_idx)
+                k2 = self.forward(x - (dt*k1)/2, t - dt/2, cls_idx)
+                k3 = self.forward(x - (dt*k2)/2, t - dt/2, cls_idx)
+                k4 = self.forward(x - dt*k3, t - dt, cls_idx)
+                x = x - (dt / 6) * (k1 + 2*k2 + 2*k3 + k4)
+        return x
+
 
 def ema(mu, dx):
     '''exponential moving average'''
     return mu*0.99 + dx*0.01 if mu else dx
 
-
 batch_size = 512
 n_steps = 8_000
-n_warmup = 100
-n_cooldown = 0 # not recommended 
+n_warmup = 100 # helps adam to get 2nd order terms right
+n_cooldown = 0 # actually hurts performance
 device = 'mps'
 
 ds = torchvision.datasets.MNIST(
@@ -93,35 +133,30 @@ dl = torch.utils.data.DataLoader(
         num_samples=batch_size*n_steps
     )
 )
-model = DenoisingMLP(
+model = ConditionalFlow(
     noise_dimension=28*28,
     condition_dimension=512,
     latent_dimension=1024,
     num_blocks=10,
+    num_classes=10,
 ).to(device)
 
-cls_tokens = torch.randn(10, 512).to(device)
+
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-6)
+schedule = torch.cat([
+    torch.linspace(0, 1, n_warmup),
+    torch.logspace(0, -1, n_steps-n_warmup+1),
+    torch.linspace(1, 0, n_cooldown),
+])
 scheduler = torch.optim.lr_scheduler.LambdaLR(
     optimizer=optimizer,
-    lr_lambda=lambda x: torch.cat([
-            torch.linspace(0, 1, n_warmup), # Adams second order need  
-            torch.logspace(0, -1, n_steps-n_warmup-n_cooldown+1),
-            torch.linspace(1, 0, n_cooldown),
-        ])[x]
+    lr_lambda=schedule.__getitem__
 )
 
-### TRAIN ###
 loss_avg = None
 for idx, (img, tar) in enumerate(dl):
-    img = img.to(device)
-    noise = torch.randn(img.shape, device=device)
-    cond = cls_tokens[tar]
-
-    time = torch.randn(size=(len(img),1), device=device).sigmoid()
-    noised = (1-time) * img + (0.001 + 0.999*time) * noise
-    pred = model(noised, time, cond)
-    loss = F.mse_loss(pred, noise.mul(0.999).sub(img))
+    img, tar = img.to(device), tar.to(device)
+    loss = model.loss(img, tar)
 
     loss.backward()
     optimizer.step()
@@ -133,35 +168,9 @@ for idx, (img, tar) in enumerate(dl):
         print(f"{idx=:04d} {loss.item()=:.9f} {loss_avg=:.9f}")
 
 
-
-
-
-### SAMPLE ###
-num_images = 16
-initial_noise = torch.randn((num_images, 28*28)).to(device)  # Start with Gaussian noise
-diffusion_steps = 100
-step_size = 1.0 / diffusion_steps
-
-idxs = torch.randint(0, 10, size=(num_images, ))
-cond = cls_tokens[idxs]
-noisy_images = initial_noise
-
-with torch.no_grad():
-    for time in tqdm(torch.linspace(1, 0, diffusion_steps, device=device)):
-        time = time.expand(num_images, 1)
-
-        # runge-kutta-4
-        k1 = model(noisy_images, time, cond)
-        k2 = model(noisy_images - (step_size*k1)/2, time - step_size/2, cond)
-        k3 = model(noisy_images - (step_size*k2)/2, time - step_size/2, cond)
-        k4 = model(noisy_images - step_size*k3, time - step_size, cond)
-        
-        noisy_images = noisy_images - (step_size / 6) * (k1 + 2*k2 + 2*k3 + k4)
-        noisy_images = noisy_images.clip(-1, 1) # images live in -1, 1
-
-
+smps = model.sample(tar, 20)
 fig, axs = plt.subplots(4, 4, figsize=(6, 6))
-for ax, xt, idx in zip(axs.flatten(), noisy_images, idxs):
+for ax, xt, idx in zip(axs.flatten(), smps[:16], tar[:16]):
     ax.imshow(xt.reshape(28, 28).detach().cpu(), vmin=-1, vmax=1)
     ax.axis("off"); ax.set_title(idx.item())
 fig.tight_layout()
